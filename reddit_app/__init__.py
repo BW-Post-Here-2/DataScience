f19cdbdf282188445263fd08154cb55e7cdcdac4
jdblackstar <josh@blackstar.dev>
2020-05-29 11:10:39 -0700

    app my_app app app

--- reddit_app/__init__.py
+++ reddit_app/__init__.py
@@ -1,3 +1,4 @@
 from .reddit_app import create_app
 
-app = create_app()
+
+create_app()
--- reddit_app/reddit_app.py
+++ reddit_app/reddit_app.py
@@ -15,7 +15,7 @@ import requests
 # Define create_app function
 def create_app():
     app = Flask(__name__)
-    
+
     # From example video.
     # Understanding flask.request
     # https://www.youtube.com/watch?v=hAEJajltHxc
@@ -25,14 +25,14 @@ def create_app():
         region = request.args["region"]
         return """<h1>The language is {}</h1>
                   <h1>The region is {}</h1>""".format(language, region)
-    
-    
+
+
     @app.route("/predict.json", methods=["POST"])
     def predict():
         # Here the request context is creating a proxy of a
         # requests object with the get_json method applied.
         request_data = request.get_json(force=True)
-    
+
         # The method used for title and content here is
         # functionally identical to the way that Flask
         # handles HTML forms Jinja passed inside, just
@@ -40,9 +40,9 @@ def create_app():
         title = request_data["title"]
         content = request_data["content"]
         post = title + " " + content
-    
+
         model = load(open("reddit_model_nc.pkl", "r+b"))
-    
+
         predictions = pd.DataFrame(model.predict_proba([post])[0])
         predictions["subreddit"] = model.classes_
         predictions.columns = ["certainty", "subreddit"]
@@ -50,7 +50,7 @@ def create_app():
         predictions = predictions.sort_values(by="certainty", ascending=False)
         predictions = predictions.reset_index()
         predictions = predictions.drop(columns=["index"])
-    
+
         # Here we have limited the list of potential
         # subreddits to 3 in order to give the user only the
         # best selection of subreddit.
@@ -62,19 +62,19 @@ def create_app():
                  "probability" : f"{round(predictions.certainty[i] * 100, 2)}"}
             )
         return jsonify(potential_subreddits)
-    
-    
+
+
     @app.route("/test")
     def test():
         title = "Reddit Post"
         content = "This is a Reddit post."
         post = {"title" : title, "content" : content}
-    
+
         url = "http://127.0.0.1:5000/predict.json"
-    
+
         req = requests.post(url, json=post)
         return req.text
-    
+
     @app.route("/")
     def base():
         strang = "Hello birthday handsome Joo Woon"
